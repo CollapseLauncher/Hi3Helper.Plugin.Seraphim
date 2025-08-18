9d734de5d012cae1643a7d6dedaf6d425b1d6905
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-08-18 15:10:53 +0700

    Adjust Core Plugin changes
    + Switch from SharedStatic to SharedStatic<T> to enable game launch API support (though, still not implemented on this repo)
    + Rename export class to Seraphim

--- Hi3Helper.Plugin.HBR/Exports.cs
+++ Hi3Helper.Plugin.HBR/Exports.cs
@@ -1,15 +1,21 @@
 ﻿using Hi3Helper.Plugin.Core;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+// ReSharper disable IdentifierTypo
+// ReSharper disable CommentTypo
 
 namespace Hi3Helper.Plugin.HBR;
 
 /// <summary>
-/// Provides necessary unmanaged API exports for the plugin to be loaded.
+/// Provides necessary unmanaged API exports for the plugin to be loaded.<br/><br/>
+/// 
+/// NOTE FOR DEVELOPERS:<br/>
+/// The export class name can be anything you want. In this example, we use "Seraphim" as a name to the weapon used by the characters.
 /// </summary>
-public class Exports : SharedStatic
+public class Seraphim : SharedStatic<Seraphim> // 2025-08-18: We use generic version of SharedStatic<T> to add support for game launch API.
+                                               //             Though, the devs can still use the old SharedStatic without any compatibility issue.
 {
-    static Exports() => Load<HBRPlugin>(!RuntimeFeature.IsDynamicCodeCompiled ? new Core.Management.GameVersion(0, 8, 1, 0) : default); // Loads the IPlugin instance as HBRPlugin.
+    static Seraphim() => Load<HBRPlugin>(!RuntimeFeature.IsDynamicCodeCompiled ? new Core.Management.GameVersion(0, 8, 1, 1) : default); // Loads the IPlugin instance as HBRPlugin.
 
     [UnmanagedCallersOnly(EntryPoint = "TryGetApiExport", CallConvs = [typeof(CallConvCdecl)])]
     public static unsafe int TryGetApiExport(char* exportName, void** delegateP) =>
