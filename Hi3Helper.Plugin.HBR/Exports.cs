92dd9d55a4aa2a3f00a5ce677c321a81c1675954
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-09-16 15:30:17 +0700

    Reset build version back to 0.8.0 to reflect test phase
    We are going to publish this plugin for testing. So we start the version of the build back to 0.8.0

--- Hi3Helper.Plugin.HBR/Exports.cs
+++ Hi3Helper.Plugin.HBR/Exports.cs
@@ -15,7 +15,7 @@ namespace Hi3Helper.Plugin.HBR;
 public partial class Seraphim : SharedStatic<Seraphim> // 2025-08-18: We use generic version of SharedStatic<T> to add support for game launch API.
                                                        //             Though, the devs can still use the old SharedStatic without any compatibility issue.
 {
-    static Seraphim() => Load<HBRPlugin>(!RuntimeFeature.IsDynamicCodeCompiled ? new Core.Management.GameVersion(0, 8, 3, 0) : default); // Loads the IPlugin instance as HBRPlugin.
+    static Seraphim() => Load<HBRPlugin>(!RuntimeFeature.IsDynamicCodeCompiled ? new Core.Management.GameVersion(0, 8, 0, 0) : default); // Loads the IPlugin instance as HBRPlugin.
 
     [UnmanagedCallersOnly(EntryPoint = "TryGetApiExport", CallConvs = [typeof(CallConvCdecl)])]
     public static unsafe int TryGetApiExport(char* exportName, void** delegateP) =>
